99e5d088cc0edb4a53ddbd42974a9df0c624619b
yunchi0825 <71751221+yunchi0825@users.noreply.github.com>
2020-11-18 16:00:25 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,21 +11,33 @@ input.onButtonPressed(Button.A, function () {
         `)
 })
 input.onGesture(Gesture.Shake, function () {
-    showled(0, 0)
-    showled(4, 0)
-    showled(0, 4)
-    showled(4, 4)
+    _00(0, 0)
+    _00(0, 4)
+    _00(4, 0)
+    _00(4, 4)
 })
-function showled (x: number, y: number) {
+function _00 (num: number, num2: number) {
     for (let I = 0; I <= 2; I++) {
         for (let j = 0; j <= I; j++) {
-            led.plot(j, I)
-            led.plot(I, j)
+            led.plot(num - Math.abs(j), num2 - Math.abs(I))
+            led.plot(num - Math.abs(I), num2 - Math.abs(j))
         }
         basic.pause(100)
     }
     basic.clearScreen()
 }
+input.onGesture(Gesture.LogoDown, function () {
+    for (let I = 0; I <= 2; I++) {
+        j = 1
+        for (let index = 0; index < 2; index++) {
+            led.plot(2 + j * I, 2)
+            led.plot(2, 2 + j * I)
+            j = j * -1
+        }
+        basic.pause(100)
+    }
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         . . . . .
@@ -46,15 +58,3 @@ input.onButtonPressed(Button.B, function () {
 })
 let j = 0
 radio.setGroup(5)
-basic.forever(function () {
-    for (let I = 0; I <= 2; I++) {
-        j = 1
-        for (let index = 0; index < 2; index++) {
-            led.plot(2 + j * I, 2)
-            led.plot(2, 2 + j * I)
-            j = j * -1
-        }
-        basic.pause(100)
-    }
-    basic.clearScreen()
-})
